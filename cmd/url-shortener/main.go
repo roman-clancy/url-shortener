01df3e37ac5c0999d4d6689f7212804396feb260
roman-clancy <roman_clancy@outlook.com>
2023-12-30 16:20:53 +0300

    Logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-    "fmt"
+    "log/slog"
+    "os"
     "url_shortener/internal/config"
 )
 
 func main() {
     cfg := config.MustLoad()
-    fmt.Println(cfg)
+    logger := setupLogger(cfg.Env)
+    logger.Info("Starting url-shortener")
+    logger.Debug("Debug level enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+    var log *slog.Logger
+    switch env {
+    case "local":
+        log = slog.New(
+            slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case "dev":
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+        )
+    case "prod":
+        log = slog.New(
+            slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+        )
+    }
+    return log
 }
